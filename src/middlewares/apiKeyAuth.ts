f759b84f8d732158020ecd0f510f4a66c2a6af17
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-08-12 16:56:32 -0300

    Update apiKeyAuth.ts

--- src/middlewares/apiKeyAuth.ts
+++ src/middlewares/apiKeyAuth.ts
@@ -4,11 +4,17 @@ import { NextFunction, Request, Response } from 'express';
 export async function authKey(req: Request, res: Response, next: NextFunction) {
     const key = req.headers['authorization']?.split(' ')[1]; // Expects "Bearer <api_key>"
     if (!key) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({
+            error: 'Unauthorized',
+            message: 'Você precisa de uma chave Bearer Token'
+        });
     }
     const retorno = await findKey(key);
     if (!retorno) {
-        return res.status(401).json({ error: 'Unauthorized 2' });
+        return res.status(401).json({
+            error: 'Unauthorized',
+            message: 'Sua chave Bearer Token é inválida'
+        });
     }
     next();
 };
