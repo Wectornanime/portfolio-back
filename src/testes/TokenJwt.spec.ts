fbd3b2d626fb4c2f2ac3e7c686218b369e5d32e2
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-08-13 12:12:15 -0300

    create first test

---
+++ src/testes/TokenJwt.spec.ts
@@ -0,0 +1,16 @@
+import { generateJwtToken, verifyJwtToken } from '@services/tokenJwt.service';
+
+describe('Verifica se o token esta sendo gerado', () => {
+    it('Gerar um token', () => {
+        const token = generateJwtToken('1');
+
+        expect(typeof token).toEqual('string');
+    });
+
+    it('Verifica o token', () =>{
+        const token = generateJwtToken('1');
+        const result = verifyJwtToken(token);
+
+        expect(typeof result).toBe('object');
+    });
+});
