aa32c6cda0f0ef7ada7eef518159e6718fc68be6
Wectornanime <wectornanime@gmail.com>
2025-12-15 13:37:13 -0300

    update the method to get all data

--- src/controllers/data/getAll-data.controller.ts
+++ src/controllers/data/getAll-data.controller.ts
@@ -2,10 +2,14 @@ import { prisma } from '@adapter/prisma.adapter';
 
 export default class GetAllInfoController implements Controller {
   async handle(): Promise<HttpResponse> {
-    const info = await prisma.info.findFirst();
-    const skills = await prisma.skill.findFirst();
-    const projects = await prisma.project.findFirst();
-    const certificates = await prisma.certificate.findFirst();
+    const info = await prisma.info.findFirst({
+      include: { links: true }
+    });
+    const skills = await prisma.skill.findMany();
+    const projects = await prisma.project.findMany({
+      include: { links: true }
+    });
+    const certificates = await prisma.certificate.findMany();
 
     const data = {
       ...info,
