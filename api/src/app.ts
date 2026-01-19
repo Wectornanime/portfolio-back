dbb9f5eb24e488f661b38b304da1f0e9bf8ece71
Wectornanime <wectornanime@gmail.com>
2026-01-19 14:47:38 -0300

    feat(api): adds the morgan to loogin the project

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+
 import { expressRouterAdapter } from '@adapter/expressRouter.adapter';
 import { appRouter } from './app.router';
 
@@ -9,6 +11,7 @@ const routes = expressRouterAdapter(appRouter);
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(morgan('dev'));
+app.use('/api', routes);
 
 export default app;
