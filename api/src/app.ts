a374b46ca9fe27ecc1af911528d5d8259646efeb
Wectornanime <wectornanime@gmail.com>
2026-01-29 17:28:29 -0300

    chore(build): add root package to build the application

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
 
 import { expressRouterAdapter } from '@adapter/expressRouter.adapter';
 import { appRouter } from './app.router';
 import { multerConfig } from './config/multer.config';
 
 const app = express();
+const clientPathDist = path.resolve(__dirname, '../../client/dist');
 
 const routes = expressRouterAdapter(appRouter);
 
@@ -14,6 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(multerConfig.single('file'));
+
 app.use('/api', routes);
+app.use(express.static(clientPathDist));
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(clientPathDist, 'index.html'));
+});
 
 export default app;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5050",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const apiUrl = env.VITE_API_URL;
+
+  return {
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: apiUrl,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
